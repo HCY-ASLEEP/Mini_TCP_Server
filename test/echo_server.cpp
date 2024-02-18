33a77d194ebe91b796e3a96631e5d17a5b659328
HCY-ASLEEP <2420066864@qq.com>
2024-02-18 06:37:46 +0000

    format code

--- test/echo_server.cpp
+++ test/echo_server.cpp
@@ -2,23 +2,25 @@
 #include "pine.h"
 
 int main() {
-  TcpServer *server = new TcpServer();
+    TcpServer *server = new TcpServer();
 
-  Signal::signal(SIGINT, [&] {
+    Signal::signal(SIGINT, [&] {
+        delete server;
+        std::cout << "\nServer exit!" << std::endl;
+        exit(0);
+    });
+
+    server->onConnect([](Connection *conn) {
+        std::cout << "New connection fd: " << conn->socket()->fd() << std::endl;
+    });
+
+    server->onRecv([](Connection *conn) {
+        std::cout << "Message from client " << conn->read_buf()->c_str() << std::endl;
+        conn->Send(conn->read_buf()->c_str());
+    });
+
+    server->Start();
+
     delete server;
-    std::cout << "\nServer exit!" << std::endl;
-    exit(0);
-  });
-
-  server->onConnect([](Connection *conn) { std::cout << "New connection fd: " << conn->socket()->fd() << std::endl; });
-
-  server->onRecv([](Connection *conn) {
-    std::cout << "Message from client " << conn->read_buf()->c_str() << std::endl;
-    conn->Send(conn->read_buf()->c_str());
-  });
-
-  server->Start();
-
-  delete server;
-  return 0;
+    return 0;
 }
